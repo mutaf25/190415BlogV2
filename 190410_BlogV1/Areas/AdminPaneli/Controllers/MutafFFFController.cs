5736ef28f6ce8856dc8e1809427d2c037d79b06e
ahmet.mutaf <ahmet.mutaf@304-L16>
2019-04-15 11:25:22 +0300

    Mutaf :D

---
+++ 190410_BlogV1/Areas/AdminPaneli/Controllers/MutafFFFController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _190410_BlogV1.Areas.AdminPaneli.Controllers
+{
+    public class MutafFFFController : Controller
+    {
+        // GET: AdminPaneli/MutafFFF
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
